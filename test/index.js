dd38d6fcd03788cede2c28c9aa3a5608ec1b420f
Fábio Santos <fabiosantosart@gmail.com>
2016-04-18 16:56:17 +0100

    feat: Add autoFocus prop to control the autofocus function.

--- test/index.js
+++ test/index.js
@@ -1,26 +1,46 @@
 import 'babel-polyfill';
-import Googlesearch from '../src';
+import GoogleSearch from '../src';
 import React from 'react';
 import chai from 'chai';
+import chaiSpies from 'chai-spies';
 import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
 chai.use(chaiEnzyme()).should();
+chai.use(chaiSpies);
 
-describe('Googlesearch', () => {
+describe('GoogleSearch', () => {
   it('is compatible with React.Component', () => {
-    Googlesearch.should.be.a('function')
+    GoogleSearch.should.be.a('function')
       .and.respondTo('render');
   });
 
   it('renders a React element', () => {
-    React.isValidElement(<Googlesearch />).should.equal(true);
+    React.isValidElement(<GoogleSearch />).should.equal(true);
   });
 
   it('checks for correct state when client-rendering', () => {
     // Shallow was used rather than mount, otherwise the component is mounted
     // twice in the example, as the Google APIs are called twice.
-    const wrapper = shallow(<Googlesearch />);
+    const wrapper = shallow(<GoogleSearch />);
     wrapper.state().useFallback.should.equal(false);
+  });
+
+  describe('focusSearchField', () => {
+    let comp = null;
+    beforeEach(() => {
+      comp = new GoogleSearch({}, {}, {});
+      comp.googleSearchInput = { focus: chai.spy() };
+    });
+    it('focuses the element when the autoFocus prop is true', () => {
+      comp.props.autoFocus = true;
+      comp.focusSearchField();
+      comp.googleSearchInput.focus.should.have.been.called.once();
+    });
+    it('doesn\'t focus the element if props.autoFocus is false', () => {
+      comp.props.autoFocus = false;
+      comp.focusSearchField();
+      comp.googleSearchInput.focus.should.not.have.been.called();
+    });
   });
 });
 
